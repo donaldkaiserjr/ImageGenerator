af707cc42875b6b8db4f8e17db8726bfd89c6b08
donald kaiser <donaldkaiserjr@gmail.com>
2021-04-25 10:46:49 -0700

    set up template html, js, and css files. Completed index.html coding

---
+++ script.js
@@ -0,0 +1,18 @@
+/* */
+// let age;
+// switch(age) {
+//     case 10:
+//         console.log('The Age is 10')
+//         break;
+//     case 20:
+//         console.log('The Age is 20')
+//         break;
+//     case 30:
+//         console.log('The Age is 30')
+//         break;
+//     default:
+//         console.log('The Age is Unknown')
+// }
+
+
+
