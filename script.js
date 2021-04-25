ee597945b66414449025e90c75c22b167f178c40
donald kaiser <donaldkaiserjr@gmail.com>
2021-04-25 11:09:52 -0700

    set up script.js with api

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-/* */
-// let age;
-// switch(age) {
-//     case 10:
-//         console.log('The Age is 10')
-//         break;
-//     case 20:
-//         console.log('The Age is 20')
-//         break;
-//     case 30:
-//         console.log('The Age is 30')
-//         break;
-//     default:
-//         console.log('The Age is Unknown')
-// }
+/*https://source.unsplash.com/   
+Simply add photo dimensions after the URL (ex: /800x600) */
 
 
+const div = document.querySelector('.container')
+const imgSrc = https://source.unsplash.com/random/
 
+function randNum(num1, num2 {
+    return Math.ceil(Math.random() * )
+}
